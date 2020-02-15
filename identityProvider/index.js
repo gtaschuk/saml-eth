b09d2f685865bc84d135a7a9ded81c7f0fe2da6e
George Weiler <georgejweiler@gmail.com>
2020-02-15 11:58:22 -0800

    use babel for es6 syntax

--- identityProvider/index.js
+++ identityProvider/index.js
@@ -7,26 +7,28 @@ const routes = require("./routes");
 const http = require("http");
 const bodyParser = require("body-parser");
 
+console.log("updated");
 
 //const origin = 'https://ranalyst.codefi.network'
-const allowedOrigins = ['http://localhost:3000', 'http://localhost']
+const allowedOrigins = ["http://localhost:3000", "http://localhost"];
 
 const corsOptions = {
   origin: function(origin, callback) {
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.' +
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg =
+        "The CORS policy for this site does not " +
+        "allow access from the specified Origin." +
         origin;
       return callback(new Error(msg), false);
     }
     return callback(null, true);
   },
   credentials: true
-}
+};
 
 app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions))
+app.options("*", cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, "build")));
 //app.use(require("cookie-parser")());
@@ -42,4 +44,6 @@ const server = http.createServer(app);
 server.listen(port, () =>
   console.log("Express server listening on port " + server.address().port)
 );
-server.on("listening", () => console.log(`Listening on port ${server.address()}`));
+server.on("listening", () =>
+  console.log(`Listening on port ${server.address()}`)
+);
