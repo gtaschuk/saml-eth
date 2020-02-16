4da3c89449186cc62bf29556f6ccd18f54c53a8a
Greg Taschuk <gregorytaschuk@gmail.com>
2020-02-16 11:37:17 -0700

    added George

--- rbac/migrations/3_add_test_users.js
+++ rbac/migrations/3_add_test_users.js
@@ -9,5 +9,8 @@ module.exports = (deployer, network, accounts) => {
     // the deploy user also has the ability to add other users to the RBAC
     await rbac.newUser("0xd5C73aFc966c456D770332411E448A550e00771b", "Test", 0);
     await rbac.grantRole("0xd5C73aFc966c456D770332411E448A550e00771b", "employee");
+
+    await rbac.newUser("0x5D3251Babe1bC525607F02E5e5b0BAEE70801559", "Test", 0);
+    await rbac.grantRole("0x5D3251Babe1bC525607F02E5e5b0BAEE70801559", "employee");
   });
 };
